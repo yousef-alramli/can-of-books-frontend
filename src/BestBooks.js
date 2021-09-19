77ba05f8397ebff36fb5aef9f142043da57703ff
yousef-alramli <ysabdys1996@gmail.com>
2021-09-19 19:45:58 +0300

    FrontEnd

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,5 +1,9 @@
 import React from 'react';
-
+import axios from 'axios';
+import {
+  Card,
+  Alert
+} from 'react-bootstrap';
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
@@ -7,21 +11,44 @@ class BestBooks extends React.Component {
       books: []
     }
   }
+  componentDidMount = () => {
+    axios.get(`http://${process.env.REACT_APP_BACKEND_URL}/books`).then(res => {
+      this.setState({
+        books: res.data.books
+      })
+    })
 
+  }
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
 
   render() {
-
     /* TODO: render user's books in a Carousel */
 
     return (
+      console.log(this.state.books),
+
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+        {this.state.books.length !== 0 ? (
+          this.state.books.map(item => {
+            return <>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>{item.title}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">{item.status}</Card.Subtitle>
+                  <Card.Text>
+                    {item.description}
+                  </Card.Text>
+                  <Card.Link href="#">{item.email}</Card.Link>
+                </Card.Body>
+              </Card>
+            </>
+          })
         ) : (
-          <h3>No Books Found :(</h3>
+          <Alert variant='danger'>
+            There's no books.
+          </Alert>
         )}
       </>
     )
