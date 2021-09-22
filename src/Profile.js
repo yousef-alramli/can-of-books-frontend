7582180c7ec82cac61ddc38291291269bc63be54
yousef-alramli <ysabdys1996@gmail.com>
2021-09-22 19:12:22 +0300

    Added Auth

--- src/Profile.js
+++ src/Profile.js
@@ -3,11 +3,17 @@ import { Component } from "react";
 class Profile extends Component {
 
   render() {
-    
+
     /* TODO: render information about logged in user */
     /* STRETCH TODO: if no logged in user then redirect home */
     return <>
-
+      {this.props.isAuthenticated &&
+        <>
+          <h1>User name: {this.props.loginAuth.name}</h1>
+        <img src={this.props.loginAuth.picture}/>
+        <h2>Email: {this.props.loginAuth.email} </h2>
+        </>
+      }
     </>
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <App />,
+  <Auth0Provider
+    domain={process.env.REACT_APP_AOUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AOUTH0_CLINTID}
+    redirectUri="http://localhost:3000"
+  >
+    <App />
+  </Auth0Provider>,
   document.getElementById("root")
 );
